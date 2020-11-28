25091107824a9401ac033319af5b68b903790f02
Sergey Pavlov <houston.inc@yandex.ru>
2020-11-28 21:54:51 +0300

    rabotaaaet

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,18 @@ import { Strategy } from "passport-discord";
 import { createConnection, getRepository } from "typeorm";
 import { User } from "./users/user.entity";
 import { usersRouter } from "./users/users";
+import passport from "passport";
+import session from "express-session";
 
-const passport = require("passport");
-const  scopes = ["identify"];
+const scopes = ["identify"];
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
 
 const bootstrap = async () => {
   try {
@@ -20,38 +29,57 @@ const bootstrap = async () => {
           clientSecret: "INO0EdLc9MgpxLaK2Wp53uuIN0W0QfZG",
           callbackURL: "/auth/discord/callback",
           scope: scopes,
+          passReqToCallback: true,
         },
         function (
+          req: express.Request,
           accessToken: string,
           refreshToken: string,
           profile: Strategy.Profile,
           cb: Function
         ) {
-          // const { id } = profile;
-          // const usersRepository = getRepository(User);
-          
-          // usersRepository.findOne();
-
-          cb(null, {});
+          process.nextTick(function () {
+            cb(null, profile);
+          });
         }
       )
     );
 
-    app.get("/auth/discord", passport.authenticate("discord"), (req, res) => {
-      console.log(req, res);
+    app.use(
+      session({
+        secret: "youshallnotpass",
+        resave: false,
+        saveUninitialized: false,
+      })
+    );
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    //@ts-ignore
+    function checkAuth(req, res, next) {
+      if (req.isAuthenticated()) return next();
+      res.send("not logged in :(");
+    }
+
+    app.get("/auth/discord", function (req, res, next) {
+      passport.authenticate("discord", { scope: scopes })(req, res, next);
     });
 
     app.get(
       "/auth/discord/callback",
       passport.authenticate("discord"),
       function (req, res) {
-        res.send("<h1>Discord auth</h1>");
+        res.redirect("/");
       }
     );
 
-    app.get("/", (req, res) => {
-      res.send("<h1>23</h1>")
-    })
+    app.get("/", checkAuth, (req, res) => {
+      if (req.user) {
+        console.log(req.user);
+      }
+      res.json(req.user);
+    });
 
     app.use("/users", usersRouter);
 
